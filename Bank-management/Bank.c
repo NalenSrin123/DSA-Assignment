07069ed23c2616a1d03109404353c275e1317b29
NalenSrin123 <161912473+NalenSrin123@users.noreply.github.com>
2024-03-26 14:37:47 +0700

    Add do while loop

--- Bank-management/Bank.c
+++ Bank-management/Bank.c
@@ -13,15 +13,17 @@ int main() {
     printf("\033[0;36m \t ########  ##     ## ##    ## ##    ##    ##     ## ##     ## ##    ## ##     ##  ######   ######## ##     ## ######## ##    ##    ##    \033[0m\n");
     printf("\n\n\n");
 
-    printf("1. Create Acoount \n");
-    printf("2. Deposit \n");
-    printf("3. Withdraw \n");
-    printf("4. Search ID \n\n");
+    do{
+        printf("1. Create Acoount \n");
+        printf("2. Deposit \n");
+        printf("3. Withdraw \n");
+        printf("4. Search ID \n\n");
 
-    printf("Option : ");
-    scanf("%d" , &option); 
+        printf("Option : ");
+        scanf("%d" , &option); 
 
-    printf("You choose : %d " , option);
+        printf("You choose : %d " , option);
+    }while(getch()==13);
 
     
     
